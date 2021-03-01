f3b884b5bd28081754c44f7278fef2e3cb7e6ffd
Luna Shuqair <luna.shuqair89@gmail.com>
2021-02-28 19:18:59 -0500

    Increased text character limit

--- models/post.js
+++ models/post.js
@@ -6,7 +6,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
       // "len" is a validation that checks that our todo is between 1 and 140 characters
       validate: {
-        len: [1, 140],
+        len: [1, 280],
       },
     },
     complete: {
